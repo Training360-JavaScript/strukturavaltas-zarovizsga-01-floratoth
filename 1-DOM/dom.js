fe9ebd9778d72860c688c94682fe0f6370d9175a
Flow <a.flora95@gmail.com>
2022-02-28 14:59:19 +0100

    manipulateDom function implemented

--- 1-DOM/dom.js
+++ 1-DOM/dom.js
@@ -1,5 +1,10 @@
 function manipulateDom() {
-    // 
-};
+  const tagWithQuoteClass = document.querySelectorAll(".quote");
+  [...tagWithQuoteClass].forEach((item) => {
+    item.style.border = "blue";
+    item.style.background = "lightblue";
+    item.style.fontStyle = "italic";
+  });
+}
 
 export { manipulateDom };
